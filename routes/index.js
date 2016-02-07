099ac0a80639cb94391e4d2ea908a4d5ef724a64
aniarya82 <hiten.iitkgp@gmail.com>
2016-02-07 09:19:14 +0530

    [js-standard] code implemented

--- routes/index.js
+++ routes/index.js
@@ -1,46 +1,46 @@
-var express = require('express');
-var passport = require('passport');
+var express = require('express')
+var passport = require('passport')
 var Account = require('../models/account.js')
-var router = express.Router();
+var router = express.Router()
 
 /* GET home page. */
-router.get('/', function(req, res) {
-  res.render('index', { user: req.user });
-});
+router.get('/', function (req, res) {
+  res.render('index', { user: req.user })
+})
 
-router.get('/register', function(req, res) {
-	res.render('register', { });
-});
+router.get('/register', function (req, res) {
+  res.render('register', { })
+})
 
-router.post('/register', function(req, res) {
-	Account.register(new Account({ username: req.body.username }), req.body.password, function(err, account) {
-		if (err) {
-			console.log(err);
-			return res.render('register', {info: "Sorry. That username already exists. Try again."});
-		}
+router.post('/register', function (req, res) {
+  Account.register(new Account({ username: req.body.username }), req.body.password, function (err, account) {
+    if (err) {
+      console.log(err)
+      return res.render('register', {info: 'Sorry. That username already exists. Try again.'})
+    }
 
-		passport.authenticate('local')(req, res, function() {
-			console.log("logs for register post data");
-			res.redirect('/');
-		});
-	});
-});
+    passport.authenticate('local')(req, res, function () {
+      console.log('logs for register post data')
+      res.redirect('/')
+    })
+  })
+})
 
-router.get('/login', function(req, res) {
-	res.render('login', { user : req.user });
-});
+router.get('/login', function (req, res) {
+  res.render('login', {user: req.user})
+})
 
-router.post('/login', passport.authenticate('local'), function(req, res) {
-	res.redirect('/');
-});
+router.post('/login', passport.authenticate('local'), function (req, res) {
+  res.redirect('/')
+})
 
-router.get('/logout', function(req, res) {
-	req.logout();
-	res.redirect('/');
-});
+router.get('/logout', function (req, res) {
+  req.logout()
+  res.redirect('/')
+})
 
-router.get('/ping', function(req, res) {
-	res.status(200).send("pong!!");
-});
+router.get('/ping', function (req, res) {
+  res.status(200).send('pong!!')
+})
 
-module.exports = router;
+module.exports = router
